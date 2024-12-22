5aec7768675e69dbdc81da8900b27ebaf8798754
Wiktor Sędzimir <wiktor.sedzimir@gmail.com>
2024-12-22 11:56:32 +0100

    Add interactive creating of planar subdivision

--- point_location/visual_kirkpatrick/showcase_utils.py
+++ point_location/visual_kirkpatrick/showcase_utils.py
@@ -46,3 +46,86 @@ def pick_point(kirkpatrick: VisualKirkpatrick):
     plt.show()
 
     return point
+
+def create_planar_subdivision():
+    """
+    Create a planar subdivision interactively using matplotlib.
+    Returns:
+    1. A list of all points (bounding polygon and inner points).
+    2. A list of tuples of indices representing edges and diagonals between points.
+    """
+    fig, ax = plt.subplots()
+    ax.set_title("Create Planar Subdivision\n1. Add bounding polygon\n2. Right-click to finish polygon\n"
+                 "3. Add points inside\n4. Right-click to finish points\n"
+                 "5. Add diagonals\n6. Right-click to finish")
+    ax.set_xlim(-10, 10)
+    ax.set_ylim(-10, 10)
+
+    all_points = []  # All entered points (bounding polygon and inner points)
+    diagonals = []  # Tuples of indices representing diagonals
+    bounding_polygon_end = [False]  # Tracks if the bounding polygon is finished
+    diagonal_start_index = [None]  # Tracks the starting point index for a diagonal
+    current_stage = [1]  # Tracks the current stage: 1=polygon, 2=points, 3=diagonals
+
+    def onclick(event):
+        if event.button == 1:  # Left-click to add a point
+            if current_stage[0] == 1:
+                # Add to bounding polygon
+                all_points.append((event.xdata, event.ydata))
+                ax.plot(event.xdata, event.ydata, 'ro')  # Red points for polygon
+                if len(all_points) > 1:
+                    ax.plot([all_points[-2][0], all_points[-1][0]],
+                            [all_points[-2][1], all_points[-1][1]], 'r-')
+                plt.draw()
+            elif current_stage[0] == 2:
+                # Add inner points
+                all_points.append((event.xdata, event.ydata))
+                ax.plot(event.xdata, event.ydata, 'bo')  # Blue points for inner points
+                plt.draw()
+            elif current_stage[0] == 3:
+                # Add diagonals
+                for i, point in enumerate(all_points):
+                    if abs(event.xdata - point[0]) < 0.5 and abs(event.ydata - point[1]) < 0.5:
+                        if diagonal_start_index[0] is None:
+                            # Set starting point for diagonal
+                            diagonal_start_index[0] = i
+                            ax.plot(point[0], point[1], 'go')  # Mark as selected
+                            plt.draw()
+                        else:
+                            # Set ending point for diagonal
+                            start = diagonal_start_index[0]
+                            end = i
+                            if start != end and (start, end) not in diagonals and (end, start) not in diagonals:
+                                diagonals.append((start, end))
+                                ax.plot([all_points[start][0], all_points[end][0]],
+                                        [all_points[start][1], all_points[end][1]], 'g--')
+                                plt.draw()
+                            diagonal_start_index[0] = None
+                        break
+
+        elif event.button == 3:  # Right-click to finish the current stage
+            if current_stage[0] == 1:
+                # Close the polygon
+                if len(all_points) > 2:
+                    # all_points.append(all_points[0])  # Close the loop
+                    ax.plot([all_points[-1][0], all_points[0][0]],
+                            [all_points[-1][1], all_points[0][1]], 'r-')
+                    plt.draw()
+                    bounding_polygon_end[0] = True
+                current_stage[0] = 2
+                ax.set_title("Add points inside the polygon\nRight-click to finish points")
+                for i in range(len(all_points) - 1):
+                    diagonals.append((i, i + 1))
+                diagonals.append((0, len(all_points) - 1))
+            elif current_stage[0] == 2:
+                current_stage[0] = 3
+                ax.set_title("Add diagonals\nRight-click to finish")
+            elif current_stage[0] == 3:
+                plt.close()
+
+    # Connect the event to the figure
+    fig.canvas.mpl_connect('button_press_event', onclick)
+    plt.show()
+
+    # Return the subdivision components
+    return all_points, diagonals
